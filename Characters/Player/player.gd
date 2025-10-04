8adfdd44222fccabe76ebbe716f28e076e8cdc0e
Marius <mariussoon2@gmail.com>
2025-10-04 12:08:00 +0300

    World with colliders

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -1,20 +1,21 @@
 extends CharacterBody3D
 
-@export var speed = 5.0
+@export var speed := 5.0
+@export var camera: Camera3D
 
 func _physics_process(_delta: float) -> void:
-    var input_vector = Vector2.ZERO
+	var input_vector = Vector2.ZERO
 
-    input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
-    input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	input_vector.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_vector.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 
-    if input_vector.length() > 0:
-        input_vector = input_vector.normalized()
-        var direction = (transform.basis * Vector3(input_vector.x, 0, input_vector.y)).normalized()
-        velocity.x = direction.x * speed
-        velocity.z = direction.z * speed
-    else:
-        velocity.x = move_toward(velocity.x, 0, speed)
-        velocity.z = move_toward(velocity.z, 0, speed)
+	if input_vector.length() > 0:
+		input_vector = input_vector.normalized()
+		var direction = (transform.basis * Vector3(input_vector.x, 0, input_vector.y)).normalized()
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.z = move_toward(velocity.z, 0, speed)
 
-    move_and_slide()
+	move_and_slide()
